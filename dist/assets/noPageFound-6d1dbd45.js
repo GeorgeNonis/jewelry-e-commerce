2f804a794a27220592ca7fa9d11179a8c7ef7d14
GeorgeNonis <georgenonis@gmail.com>
2023-11-08 02:15:43 +0100

    Adding dist

---
+++ dist/assets/noPageFound-6d1dbd45.js
@@ -0,0 +1,1 @@
+import{j as e,J as s,L as n}from"./index-19b8ef38.js";const a=()=>e.jsxs("main",{className:s.main,children:[e.jsx("h3",{children:"Requested page does not exist!"}),e.jsx("h3",{children:"Head back"}),e.jsx(n,{to:"/",children:e.jsx("button",{children:"Home"})})]});export{a as default};
