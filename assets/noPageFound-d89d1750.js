db7186b01b61b43d8edb7f2241b746f9a255ac96
GeorgeNonis <georgenonis@gmail.com>
2023-11-08 01:35:28 +0100

    Updates

---
+++ assets/noPageFound-d89d1750.js
@@ -0,0 +1,1 @@
+import{j as e,J as s,L as n}from"./index-a7dbd6ae.js";const a=()=>e.jsxs("main",{className:s.main,children:[e.jsx("h3",{children:"Requested page does not exist!"}),e.jsx("h3",{children:"Head back"}),e.jsx(n,{to:"/",children:e.jsx("button",{children:"Home"})})]});export{a as default};
